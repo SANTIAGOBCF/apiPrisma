448a077c25f43039f6acdfac6c2c0a895c3bc84b
SANTIAGOBCF <58187994+SANTIAGOBCF@users.noreply.github.com>
2023-12-08 18:10:50 -0500

    2da parte

--- src/index.js
+++ src/index.js
@@ -4,13 +4,22 @@ import orderRoutes from "./routes/orders.routes.js";
 import cors from "cors";
 
 const app = express();
-// Allow requests from localhost
-/*const corsOptions = {
-    origin: 'http://localhost:3000', // Replace with your desired port number
-  };
-  */
-app.use(cors());
 
+var allowedOrigins = ['http://localhost:4200',
+                      'http://yourapp.com'];
+app.use(cors({
+  origin: function(origin, callback){
+    // allow requests with no origin 
+    // (like mobile apps or curl requests)
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
 app.use(express.json());
 
 app.use("/api", productRoutes);
